0cd6e42ed691ddda8a09ddf986b0b03570d61019
Prabin <prabinroy1@gmail.com>
2024-08-01 06:58:41 +0530

    changed the base url axios base instance

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://192.168.29.88:3500', // use 'http://localhost:3500/' to make webosocket function properly on a mobile device connected to a localhost server
+  baseURL: 'https://13.201.115.88:3500', //http://192.168.29.88:3500 use 'http://localhost:3500/' to make webosocket function properly on a mobile device connected to a localhost server
   headers: {
     //'Content-Type': 'application/json',
     'Accept': 'application/json',
