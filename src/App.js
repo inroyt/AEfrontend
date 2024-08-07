d4480698f330554f4ff57a1ff642018aeb2a8bbd
Prabin <prabinroy1@gmail.com>
2024-08-07 12:07:14 +0530

    modified the websocket link

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     let ws = null;
   
     if (!socket && profile?.linkname !== undefined) {
-      ws = new WebSocket(`ws:///13.201.115.88:3500/chat?userId=${profile?.linkname}`);
+      ws = new WebSocket(`ws:https://assamemployment.org:3500/chat?userId=${profile?.linkname}`);
   
       ws.onopen = () => {
        // console.log('WebSocket connected');
